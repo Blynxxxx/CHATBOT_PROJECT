2bf61d6be83e123e5ecd10a6ecdd8ea51d30e845
Linh Van <baolinh.van@my.jcu.edu.au>
2025-03-27 09:23:12 +0800

    Set up packages and basic interface

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from dotenv import load_dotenv
+import os
+from PyPDF2 import PdfReader
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, GoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+
+# Load environment variables
+load_dotenv()
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+def initialize_sidebar():
+    """Set up the sidebar contents."""
+    with st.sidebar:
+        st.title('Orientation Chatbot')
+        st.markdown('''
+        ## About
+        This app is an LLM-powered chatbot built using:
+        - [StreamLit](https://streamlit.io/)
+        - [LangChain](https://python.langchain.com/)
+        - [Google Gemini](https://ai.google.dev/)
+        ''')
+        add_vertical_space(5)
+        st.write('Created by Group-8')
+        
